d56581e33ca73047d23bf7bf15a71ccf1202a2e6
emanuele <emanu.alterio@gmail.com>
2025-03-05 12:14:43 +0100

    Ulteriori modifiche ai filtri di ricerca. Aggiunto valore 'qualsiasi' alle checkbox

--- src/frontend/src/elements/FilterComponent/FilterComponent.jsx
+++ src/frontend/src/elements/FilterComponent/FilterComponent.jsx
@@ -28,15 +28,51 @@ export default function FilterComponent({ initialFilters = {} }) {
   const [rooms, setRooms] = useState(0);
   const [bathrooms, setBathrooms] = useState(0);
   const [balcony, setBalcony] = useState(0);
-  const [energyclass, setEnergyClass] = useState("A");
+  const [energyclass, setEnergyClass] = useState("");
   const [price, setPrice] = useState([10000, 1000000]);
   const [size, setSize] = useState([20, 2000]);
-  const [garage, setGarage] = useState(false);
-  const [garden, setGarden] = useState(false);
-  const [elevator, setElevator] = useState(false);
-  const [climate, setClimate] = useState(false);
-  const [terrace, setTerrace] = useState(false);
-  const [reception, setReception] = useState(false);
+  const [garage, setGarage] = useState("qualsiasi");
+  const [garden, setGarden] = useState("qualsiasi");
+  const [elevator, setElevator] = useState("qualsiasi");
+  const [climate, setClimate] = useState("qualsiasi");
+  const [terrace, setTerrace] = useState("qualsiasi");
+  const [reception, setReception] = useState("qualsiasi");
+  const [category, setCategory] = useState({ value: "BUY", label: "Vendita" });
+
+
+  const categoriaOptions = [
+    { value: "BUY", label: "Vendita" },
+    { value: "RENT", label: "Affitto" }, 
+  ];
+
+  const balconyOptions = Array.from({ length: 6 }, (_, i) => ({
+    value: i,
+    label: i === 0 ? "Nessuna preferenza per i balconi" : `${i} Balcon${i > 1 ? 'i' : 'e'}`,
+  }));
+
+  const bathroomOptions = Array.from({ length: 6 }, (_, i) => ({
+    value: i,
+    label: i === 0 ? "Nessuna preferenza per i bagni" : `${i} Bagn${i > 1 ? 'i' : 'o'}`,
+  }));
+
+  const roomOptions = Array.from({ length: 6 }, (_, i) => ({
+    value: i,
+    label: i === 0 ? "Nessuna preferenza per le camere" : `${i} Camer${i > 1 ? 'e' : 'a'}`,
+  }));
+
+  const energyClassOptions = [
+    { value: "", label: "Qualsiasi classe"},
+    { value: "A4", label: "A4" },
+    { value: "A3", label: "A3" },
+    { value: "A2", label: "A2" },
+    { value: "A", label: "A" },
+    { value: "B", label: "B" },
+    { value: "C", label: "C" },
+    { value: "D", label: "D" },
+    { value: "E", label: "E" },
+    { value: "F", label: "F" },
+    { value: "G", label: "G" },
+  ];
 
   const regionOptions = regionsData.map((region) => ({
     value: region.codice_regione,
@@ -61,16 +97,23 @@ export default function FilterComponent({ initialFilters = {} }) {
           }))
       : [];
 
+
+  const handleCheckboxChange = (setState) => (e) => {
+    setState(e.target.checked ? true : "qualsiasi");
+  };
+    
+
   const handleFilter = async () => {
     const filters = {
       region: selectedRegion ? selectedRegion.label : null,
       province: selectedProvince ? selectedProvince.label : null,
       municipality: selectedMunicipality ? selectedMunicipality.label : null,
+      contract: category.value,
       // price: price[0],
-      // room: Number(rooms),
-      // bathroom: Number(bathrooms),
-      //balcony: Number(balcony),
-      energyclass,
+      room: Number(rooms.value),
+      bathroom: Number(bathrooms.value),
+      balcony: Number(balcony.value),
+      energyclass: energyclass,
       // surface: size[0],
       // garage,
       // garden,
@@ -81,6 +124,7 @@ export default function FilterComponent({ initialFilters = {} }) {
     };
 
     try {
+      console.log(filters);
       navigate(`/filtered-search?ts=${new Date().getTime()}`, { state: filters });
     } catch (error) {
       console.error("Errore nel filtraggio:", error);
@@ -101,6 +145,12 @@ export default function FilterComponent({ initialFilters = {} }) {
       {showFilters && (
         <>
           <div className="select-group">
+            <Select 
+              options={categoriaOptions} 
+              value={category} 
+              onChange={setCategory} 
+              className="select"
+            />
             <Select 
               options={regionOptions} 
               placeholder="Regione" 
@@ -134,62 +184,34 @@ export default function FilterComponent({ initialFilters = {} }) {
           </div>
           {/* Altri filtri */}
           <div className="select-group">
-            <div className="input-group">
-              <CustomSelect
-                label="Stanza"
-                value={rooms}
-                onChange={(e) => setRooms(e.target.value)}
-                defaultStyle="dropdown"
-              >
-                {[0, 1, 2, 3, 4, 5].map((num) => (
-                  <option key={num} value={num}>
-                    {num}
-                  </option>
-                ))}
-              </CustomSelect>
-            </div>
-            <div className="input-group">
-              <CustomSelect
-                label="Bagni"
-                value={bathrooms}
-                onChange={(e) => setBathrooms(e.target.value)}
-                defaultStyle="dropdown"
-              >
-                {[0, 1, 2, 3, 4, 5].map((num) => (
-                  <option key={num} value={num}>
-                    {num}
-                  </option>
-                ))}
-              </CustomSelect>
-            </div>
-            <div className="input-group">
-              <CustomSelect
-                label="Balconi"
-                value={balcony}
-                onChange={(e) => setBalcony(e.target.value)}
-                defaultStyle="dropdown"
-              >
-                {[0, 1, 2, 3, 4, 5].map((num) => (
-                  <option key={num} value={num}>
-                    {num}
-                  </option>
-                ))}
-              </CustomSelect>
-            </div>
-            <div className="input-group">
-              <CustomSelect
-                label="Classe Energetica"
-                value={energyclass}
-                onChange={(e) => setEnergyClass(e.target.value)}
-                defaultStyle="dropdown"
-              >
-                {["G", "F", "E", "D", "C", "B", "A", "A2", "A3", "A4"].map((option) => (
-                  <option key={option} value={option}>
-                    {option}
-                  </option>
-                ))}
-              </CustomSelect>
-            </div>
+            <Select
+              options={roomOptions}
+              value={rooms}
+              onChange={setRooms}
+              placeholder="Numero Camere"
+              className="select"
+            />
+            <Select
+              options={bathroomOptions}
+              value={bathrooms}
+              onChange={setBathrooms}
+              placeholder="Numero Bagni"
+              className="select"
+            />
+            <Select
+              options={balconyOptions}
+              value={balcony}
+              onChange={setBalcony}
+              placeholder="Numero Balconi"
+              className="select"
+            />
+            <Select
+              options={energyClassOptions}
+              value={energyClassOptions.find(option => option.value === energyclass) || null}
+              onChange={(selectedOption) => setEnergyClass(selectedOption.value)}
+              placeholder="Classe Energetica"
+              className="select"
+            />
           </div>
           <div className="range-group">
             <RangeSlider
@@ -213,22 +235,22 @@ export default function FilterComponent({ initialFilters = {} }) {
           </div>
           <div className="checkbox-group">
             <div className="toggle-group">
-              <CheckBox label="Garage" checked={garage} onChange={() => setGarage(!garage)} />
+              <CheckBox label="Garage" checked={garage === true} onChange={handleCheckboxChange(setGarage)} />
             </div>
             <div className="toggle-group">
-              <CheckBox label="Giardino" checked={garden} onChange={() => setGarden(!garden)} />
+              <CheckBox label="Giardino" checked={garden === true} onChange={handleCheckboxChange(setGarden)} />
             </div>
             <div className="toggle-group">
-              <CheckBox label="Ascensore" checked={elevator} onChange={() => setElevator(!elevator)} />
+              <CheckBox label="Ascensore" checked={elevator === true} onChange={handleCheckboxChange(setElevator)} />
             </div>
             <div className="toggle-group">
-              <CheckBox label="Climatizzazione" checked={climate} onChange={() => setClimate(!climate)} />
+              <CheckBox label="Climatizzazione" checked={climate === true} onChange={handleCheckboxChange(setClimate)} />
             </div>
             <div className="toggle-group">
-              <CheckBox label="Terrazzo" checked={terrace} onChange={() => setTerrace(!terrace)} />
+              <CheckBox label="Terrazzo" checked={terrace === true} onChange={handleCheckboxChange(setTerrace)} />
             </div>
             <div className="toggle-group">
-              <CheckBox label="Portineria" checked={reception} onChange={() => setReception(!reception)} />
+              <CheckBox label="Portineria" checked={reception === true} onChange={handleCheckboxChange(setReception)} />
             </div>
           </div>
           <Button label="Filtra" defaultStyle="login" onClick={handleFilter}>
